728f4f4d88c9908c251def3a03b404c6fdbefa7c
Adrian Hass <adrian.hass@gmail.com>
2014-07-21 22:53:07 +0200

    downloading data

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+# get source data from web
+downloadSourceData <- function() {
+  remoteUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  localFilename <<- "UCI-HAR-Dataset.zip"
+  if (!file.exists(localFilename)) {
+    download.file(
+      remoteUrl,
+      localFilename,
+      method="curl"
+    )      
+  }
+}
+
+downloadSourceData()
