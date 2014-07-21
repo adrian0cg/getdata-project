a4bef7d86d17a62bde5f86a72c3b2ec531dee8d1
Adrian Hass <adrian.hass@gmail.com>
2014-07-21 23:58:32 +0200

    * load output data (activities) * convert activities into factors

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,8 @@
+# util functions
+dataRelativePath <- function (trainOutputFilename) {
+  paste(dataDir,trainOutputFilename,sep="/")
+}
+
 # get source data from web
 downloadSourceData <- function() {
   remoteUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -11,9 +16,9 @@ downloadSourceData <- function() {
   }
 }
 downloadSourceData()
-
+# prepare source data
 extractSourceData <- function() {
-  dataDir <- "data"
+  dataDir <<- "data"
   unzip(
     localFilename,
     exdir = dataDir,
@@ -22,3 +27,33 @@ extractSourceData <- function() {
   )
 }
 extractSourceData()
+
+# start assembly of label output (y) data frame
+readOutputData <- function() {
+  testOutputFilename <- "y_test.txt"
+  trainOutputFilename <- "y_train.txt"
+  testOutput <- read.table( dataRelativePath(testOutputFilename))
+  trainOutput <- read.table( dataRelativePath(trainOutputFilename))
+  output <<- rbind(trainOutput, testOutput)
+}
+readOutputData()
+# annotate the output labels with corresponding activity labels
+annotateOutputWithFactors <- function() {
+  labelFilename <- "activity_labels.txt"
+  outputLabels <- read.table(
+    dataRelativePath(labelFilename),
+    stringsAsFactors = TRUE
+  )
+  activities <<- merge( output, outputLabels, by.y = "V1", all.x = TRUE)
+}
+annotateOutputWithFactors()
+
+# start assembly of input (X) data frame
+readInputData <- function() {
+  testInputFilename <- "X_test.txt"
+  trainInputFilename <- "X_train.txt"
+  testInput <- read.table( dataRelativePath(testInputFilename))
+  trainInput <- read.table( dataRelativePath(trainInputFilename))
+  input <<- rbind(trainInput, testInput)
+}
+#readInputData()
